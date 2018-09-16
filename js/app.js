e11e5fe54dae41d9b91c5ab0c2114a9d609b624f
Jadran Vulinovic <jvulinovic@gmail.com>
2018-09-16 15:08:25 +1000

    Enemy addition and movement completed

--- js/app.js
+++ js/app.js
@@ -1,29 +1,45 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function() {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if (this.x > 500) {
+      this.x = -50;
+      this.speed = Math.floor(Math.random() * 400) + 100;
+    }
 };
+
+
+// Set enemy starting positions
+
+//y co-ordinates of enemies
+enemyPosition = [63, 145, 227];
+
+var allEnemies = [];
+
+enemyPosition.forEach(function(position){
+  allEnemies.push(new Enemy(x=0, y=position, this.speed = Math.floor(Math.random() * 400) + 100))
+});
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+
+
 
 
 // Now instantiate your objects.
@@ -42,5 +58,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    //player.handleInput(allowedKeys[e.keyCode]);
 });
